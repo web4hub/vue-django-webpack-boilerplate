a4631e14e6a6a489b4e73253356b2924623cc7be
web4 <lunghugh@gmail.com>
2026-09-23 02:37:32 +0000

    Update deploy-docs.sh

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -5,4 +5,8 @@ cd _book
 git init
 git add -A
 git commit -m 'update book'
-git push -f git@github.com:NdagiStanley/vue-django.git master:gh-pages
+git push -f git@github.com:web4hub/vue-django-webpack-boilerplate.git main:github-pages
+git branch -m dependabot/pip/template/coveralls-3.2.0 main
+git fetch origin
+git branch -u origin/main main
+git remote set-head origin -a
