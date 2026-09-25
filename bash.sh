ca2f2912fe876aad011ae00d036dfaa8834c1113
web4 <lunghugh@gmail.com>
2026-09-25 12:50:53 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,3 +5,8 @@ git commit -m "My first commit"
 heroku create -a vue-django-webpack-boilerplate
 git remote -v
 git push heroku main
+$ npm install -g vue-cli
+$ vue init webpack my-project
+$ cd my-project
+$ npm install
+$ npm run dev
